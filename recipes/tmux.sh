416a7528e4c4df501e0c63e97ca11337abd84a69
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2021-04-27 19:37:19 +0300

    Update inventory function

--- recipes/tmux.sh
+++ recipes/tmux.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 function install {
+  VERSION="2.8"
   TMUX_PATH="$HOME/opt/tmux"
   rm -rf $TMUX_PATH
 
   # sudo apt-get -y install pkg-config libevent-dev libncurses5-dev libncursesw5-dev
 
-  git clone https://github.com/tmux/tmux.git "$TMUX_PATH"
+  git clone --depth 1 --branch "$VERSION" https://github.com/tmux/tmux.git "$TMUX_PATH"
   cd "$TMUX_PATH"
-  git checkout 2.8
 
   sh autogen.sh
   sh configure && make
   cp tmux $HOME/bin
+}
+
+function dependencies {
+  brew install reattach-to-user-namespace
 }
 
 function cpu_monitor {
--- shewill/functions.sh
+++ shewill/functions.sh
@@ -11,7 +11,9 @@ function ssh-agent-start {
 function refresh_inventory {
   find /home/justas/repos/ansible/inventory* -type f -exec cat {} \; | \
     grep -Po ".*ansible_host=[\d\.]*" | \
+    sed 's/^#.*//' | \
     sed 's/ansible_host=//' |
-    sed 's/^#//' | \
+    sed '/^$/d' |
+    sed 's/\s/ root@/' |
     sort -u > /home/justas/repos/inventory
 }
